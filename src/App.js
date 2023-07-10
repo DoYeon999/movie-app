59593dd8d38a4622085589defa3dc4b76f2243a1
drv98 <wkdal2965@naver.com>
2023-07-10 15:12:27 +0900

    추가 제거 마무리

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading.jsx";
-import SearchBox from "./components/SearchBox";
 import ScrollContainer from "react-indiana-drag-scroll";
+import SearchBox from "./components/SearchBox ";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [favourites, setFavourites] = useState([]);
-  const [searchValue, setSearchValue] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   //검색어로 영화데이터 요청
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=871d1d61`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=581b2060`;
 
     //자바스크립트는 비동기이므로 await를 붙여서 영화데이터를 다바도 다음코드 실행
     //async/awit 함께 사용 둘중하나 없으면 에러
@@ -52,6 +52,16 @@ function App() {
     saveToLocalStorage(newList); //저장하기
   };
 
+  //선호작을 제거하는 함수(id가 같으면 필터로 제거)
+  const removeMovie = (movie) => {
+    const newList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newList);
+    saveToLocalStorage(newList);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row align-items-center my-4 ">
@@ -59,11 +69,22 @@ function App() {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <ScrollContainer className="row scroll-container">
-        <MovieList movies={movies} handleClick={addFavouriteMovie} />
+        <MovieList
+          movies={movies}
+          handleClick={addFavouriteMovie}
+          addMovie={true}
+        />
       </ScrollContainer>
       <div className="row align-items-center my-4 ">
         <MovieListHeading heading="내선호작" />
       </div>
+      <ScrollContainer className="row scroll-container">
+        <MovieList
+          movies={favourites}
+          handleClick={removeMovie}
+          addMovie={false}
+        />
+      </ScrollContainer>
     </div>
   );
 }
